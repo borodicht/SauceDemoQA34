181d81f163cab77ddfc30d1f5cbccab1dbdb072e
Borodich Timofei <borodich.timofei@gmail.com>
2026-05-13 20:08:28 +0300

    added page object

--- src/test/java/CartTest.java
+++
@@ -1,9 +0,0 @@
-import org.testng.annotations.Test;
-
-public class CartTest extends BaseTest {
-
-    @Test
-    public void checkCart() {
-
-    }
-}
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,13 @@
+package tests;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pages.CartPage;
+import pages.LoginPage;
+import pages.ProductsPage;
 
 import java.time.Duration;
 import java.util.HashMap;
@@ -10,6 +15,9 @@ import java.util.HashMap;
 public class BaseTest {
 
     WebDriver driver;
+    LoginPage loginPage;
+    ProductsPage productsPage;
+    CartPage cartPage;
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() {
@@ -26,6 +34,10 @@ public class BaseTest {
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        loginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
+        cartPage = new CartPage(driver);
     }
 
     @AfterMethod(alwaysRun = true)
